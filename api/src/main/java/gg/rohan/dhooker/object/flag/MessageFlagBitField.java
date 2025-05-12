b7db2b9f5117ab5b9284b41eb4bb3c8394175137
RG <76780190+AV3RG@users.noreply.github.com>
2025-05-13 02:19:35 +0530

    feat(message-flag): static overloads

--- api/src/main/java/gg/rohan/dhooker/object/flag/MessageFlagBitField.java
+++ api/src/main/java/gg/rohan/dhooker/object/flag/MessageFlagBitField.java
@@ -1,5 +1,6 @@
 package gg.rohan.dhooker.object.flag;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MessageFlagBitField {
@@ -7,6 +8,7 @@ public class MessageFlagBitField {
     private final int bitField;
 
     public MessageFlagBitField(final List<MessageFlagType> flags) {
+        // No need to use immutables here, the list is read once and then not used again
         int bitField = 0;
         for (final MessageFlagType flag : flags) {
             bitField |= (1 << flag.getBit());
@@ -20,6 +22,14 @@ public class MessageFlagBitField {
 
     public boolean isSet(final MessageFlagType flag) {
         return (this.bitField & 1 << flag.getBit()) == (1 << flag.getBit());
+    }
+
+    public static MessageFlagBitField messageFlagBitField(final List<MessageFlagType> flags) {
+        return new MessageFlagBitField(flags);
+    }
+
+    public static MessageFlagBitField messageFlagBitField(final MessageFlagType... flags) {
+        return new MessageFlagBitField(Arrays.asList(flags));
     }
 
 }
