32e08b7d70fffac422627a48421ef2f6e65c34c7
RG <76780190+AV3RG@users.noreply.github.com>
2025-05-13 15:38:46 +0530

    fix(channel-type): identifier

--- api/src/main/java/gg/rohan/dhooker/object/component/select/ChannelType.java
+++ api/src/main/java/gg/rohan/dhooker/object/component/select/ChannelType.java
@@ -4,24 +4,29 @@ import gg.rohan.dhooker.serialization.IdentifiedEnum;
 
 public enum ChannelType implements IdentifiedEnum<Integer> {
 
-    GUILD_TEXT,
-    DM,
-    GUILD_VOICE,
-    GROUP_DM,
-    GUILD_CATEGORY,
-    GUILD_ANNOUNCEMENT,
-    ANNOUNCEMENT_THREAD,
-    PUBLIC_THREAD,
-    PRIVATE_THREAD,
-    GUILD_STAGE_VOICE,
-    GUILD_DIRECTORY,
-    GUILD_FORUM,
-    GUILD_MEDIA,
+    GUILD_TEXT(0),
+    DM(1),
+    GUILD_VOICE(2),
+    GROUP_DM(3),
+    GUILD_CATEGORY(4),
+    GUILD_ANNOUNCEMENT(5),
+    ANNOUNCEMENT_THREAD(10),
+    PUBLIC_THREAD(11),
+    PRIVATE_THREAD(12),
+    GUILD_STAGE_VOICE(13),
+    GUILD_DIRECTORY(14),
+    GUILD_FORUM(15),
+    GUILD_MEDIA(16),
     ;
 
+    private final int id;
+
+    ChannelType(final int id) {
+        this.id = id;
+    }
 
     @Override
     public Integer getIdentifier() {
-        return this.ordinal();
+        return this.id;
     }
 }
